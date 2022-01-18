16404891fbdbb2ca6140bf44a862924483502189
M K Gharzai <kabeer@gharzai.net>
2022-01-18 10:45:42 -0800

    updated fish config with starship, timestamps, pfetch

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,20 @@
 if status is-interactive
-	#echo -e "\e[95m"(date +%c)"\e[97m"
-	#echo -e "\x1b[38;2;0;112;248m"(date +%X%x)"\x1b[0m"
-	echo -e "\x1b[38;2;0;112;248m"(date +%c)"\x1b[0m"
-	alias ls='exa'
+    alias ls='exa'
+    alias ll='ls -l'
+    alias l1='ls -1'
+    alias l='l1'
 	alias l='ls'
+	
+	# pip --user paths
 	#set -Ux PYTHONPATH (python -c "import site; print(site.USER_SITE)")
+	
+	# gentoo
+	echo -e "\x1b[38;2;0;112;248m"(date +%c)"\x1b[0m"
+	
+	# macos
+    #pfetch
+    #echo -e "\033[0;32m"(date +%c)"\x1b[0m"
+    
+    # starship init
+    #source (/usr/local/bin/starship init fish --print-full-init | psub)
 end
