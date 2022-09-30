2aaf5c62f688047ac474965a933c61b7f94374e2
M K Gharzai <kabeer@gharzai.net>
2022-09-30 16:02:08 -0700

    updated ranger for js mimetype

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -130,7 +130,7 @@ handle_mime() {
     local mimetype="${1}"
     case "${mimetype}" in
         # Text
-        text/* | */xml | */json)
+        text/* | */xml | */json | application/javascript)
             # Syntax highlight
             if [[ "$( stat --printf='%s' -- "${FILE_PATH}" )" -gt "${HIGHLIGHT_SIZE_MAX}" ]]; then
                 exit 2
