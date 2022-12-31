a5eb1d2cddb855d528e736e0454b5c9a9c5b600f
M K Gharzai <kabeer@gharzai.net>
2022-12-30 16:32:27 -0800

    updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,22 @@
 if status is-interactive
-    alias ls='exa'
-    alias ll='ls -l'
-    alias l1='ls -1'
-    alias l='l1'
-	alias l='ls'
-	
-	# pip --user paths
-	#set -Ux PYTHONPATH (python -c "import site; print(site.USER_SITE)")
-	
-	# gentoo
-	echo -e "\x1b[38;2;0;112;248m"(date +%c)"\x1b[0m"
-	
-	# macos
-    #pfetch
+    alias ls="exa"
+    alias l="ls"
+    alias ll="ls -l"
+    alias l1="ls -1"
+    alias vim="nvim"
+    alias vimdiff="nvim -d"
+
+    # pip --user paths
+    #set -Ux PYTHONPATH (python -c "import site; print(site.USER_SITE)")
+    #set -Ux SCIPY_PIL_IMAGE_VIEWER sxiv
+
+    pfetch
+    # gentoo
+    echo -e "\x1b[38;2;0;112;248m"(date +%c)"\x1b[0m"
+
+    # macos
     #echo -e "\033[0;32m"(date +%c)"\x1b[0m"
-    
+
     # starship init
     #source (/usr/local/bin/starship init fish --print-full-init | psub)
 end
