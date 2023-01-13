de9ba73f36d22bccb295866224011ad9bf9076b4
M K Gharzai <kabeer@gharzai.net>
2023-01-13 05:51:09 -0800

    restored startup theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -209,7 +209,7 @@ vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.o.termguicolors = true
--- vim.cmd[[colorscheme tokyonight-night]]
+vim.cmd[[colorscheme tokyonight-night]]
 -- vim.cmd[[colorscheme onedark]]
 
 -- Transparent background
