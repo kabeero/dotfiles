db2dedabc0aa7574f77d3a40adc9e5080ece2615
M K Gharzai <kabeer@gharzai.net>
2023-12-04 09:07:23 -0800

    disable pairs, gitsigns patch

--- nvim/lazy/lua/plugins/plugins.lua
+++ nvim/lazy/lua/plugins/plugins.lua
@@ -1,6 +1,8 @@
 return {
   { "catppuccin/nvim" },
+  { "echasnovski/mini.pairs", enabled = false },
   { "f-person/git-blame.nvim" },
+  { "lewis6991/gitsigns.nvim", opts = { _extmark_signs = false } },
   { "nvim-tree/nvim-tree.lua", enabled = false },
   { "s1n7ax/nvim-window-picker" },
   { "tpope/vim-fugitive" },
