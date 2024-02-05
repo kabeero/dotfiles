c004c01ef862763ef0626591386d76852d154243
M K Gharzai <kabeer@gharzai.net>
2024-02-04 18:07:06 -0800

    upgrade nvim fix ibl

--- nvim/lazy/lua/plugins/plugins.lua
+++ nvim/lazy/lua/plugins/plugins.lua
@@ -3,7 +3,6 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   { "f-person/git-blame.nvim" },
   { "fynnfluegge/monet.nvim" },
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "lewis6991/gitsigns.nvim", opts = { _extmark_signs = false } },
   { "NeogitOrg/neogit", dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" }, config = true },
   { "nvim-telescope/telescope-symbols.nvim" },
