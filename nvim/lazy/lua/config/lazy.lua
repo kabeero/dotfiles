fa54148c273067eafc1cc441ac635cd043cf73f0
M K Gharzai <kabeer@gharzai.net>
2024-06-22 17:33:48 -0700

    nvim: colorscheme

--- nvim/lazy/lua/config/lazy.lua
+++ nvim/lazy/lua/config/lazy.lua
@@ -9,7 +9,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "carbonfox" } },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.codeium" },
