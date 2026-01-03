f00dd89a3396356ecad84e934d9adeec19864cfc
M K Gharzai <kabeer@gharzai.net>
2026-01-02 22:23:35 -0800

    fish: global editor

--- fish/config.fish
+++ fish/config.fish
@@ -19,7 +19,7 @@ if status is-interactive
 
     abbr gitclean "git fetch -p && git branch -vv | grep ': gone]' | awk '{print}' | xargs git branch -D $argv;"
 
-    set -Ux EDITOR nvim
+    set -gx EDITOR nvim
 
     set -Ux GOPATH {$HOME}/code/go
     set -Ux GOBIN {$GOPATH}/bin
