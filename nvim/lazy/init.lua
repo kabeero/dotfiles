4eb946d76c1097004dcc280101cbc5c0f3751fcc
M K Gharzai <kabeer@gharzai.net>
2026-06-10 17:56:13 +0100

    fix(nvim): gitignore and hidden

--- nvim/lazy/init.lua
+++ nvim/lazy/init.lua
@@ -1,12 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-require("telescope").setup({
-  pickers = {
-    live_grep = {
-      additional_args = function(opts)
-        return { "--no-ignore" }
-      end,
-    },
-  },
-})
--- nvim/lazy/lua/plugins/plugins.lua
+++ nvim/lazy/lua/plugins/plugins.lua
@@ -16,7 +16,21 @@ return {
   { "f-person/git-blame.nvim" },
   { "folke/tokyonight.nvim", lazy = false },
   { "fynnfluegge/monet.nvim", lazy = false },
-  { "ibhagwan/fzf-lua", keys = { { "<leader>fg", false } } },
+  {
+    "ibhagwan/fzf-lua",
+    keys = { { "<leader>fg", false } },
+    opts = function(_, opts)
+      -- Find Files
+      opts.files = opts.files or {}
+      opts.files.fd_opts = "--color=never --type f --hidden --follow --no-ignore --exclude .git"
+      opts.files.rg_opts = "--color=never --files --hidden --follow --no-ignore -g '!.git'"
+
+      -- Live Grep
+      opts.grep = opts.grep or {}
+      opts.grep.rg_opts =
+        "--column --line-number --no-heading --color=always --smart-case --max-columns=4096 --hidden --no-ignore -e"
+    end,
+  },
   { "Isrothy/neominimap.nvim" },
   {
     "kylechui/nvim-surround",
@@ -38,7 +52,32 @@ return {
   },
   { "nvim-neo-tree/neo-tree.nvim", opts = { window = { position = "float" } } },
   { "nvim-telescope/telescope-symbols.nvim" },
-  { "nvim-telescope/telescope.nvim", lazy = false },
+  {
+    "nvim-telescope/telescope.nvim",
+    lazy = false,
+    opts = function(_, opts)
+      opts.defaults = opts.defaults or {}
+      opts.defaults.vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--no-ignore",
+        "--hidden",
+      }
+      opts.pickers = opts.pickers or {}
+      opts.pickers.live_grep = opts.pickers.live_grep or {}
+      opts.pickers.live_grep.additional_args = function()
+        return { "--no-ignore", "--hidden" }
+      end
+      opts.pickers.find_files = opts.pickers.find_files or {}
+      opts.pickers.find_files.hidden = true
+      opts.pickers.find_files.no_ignore = true
+    end,
+  },
   { "nvim-tree/nvim-tree.lua", enabled = false },
   { "s1n7ax/nvim-window-picker" },
   { "tpope/vim-fugitive" },
